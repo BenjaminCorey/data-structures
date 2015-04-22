5df35194adf7b104fe8a8c79e80da9e5b7f6fa62
Benjamin Wilkins <becowilk@gmail.com>
2015-04-21 17:04:50 -0700

    stub out a hash table

---
+++ javascript/hash_tables/hash_table_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var HashTable = require('./hash_table');
+
+describe('HashTable', function() {
+  it('is defined', function() {
+    expect(typeof HashTable).to.equal('function');
+  });
+});
+
